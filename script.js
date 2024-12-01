23cdbb4f8b6f77f0ff71a55018683299d519192a
Zixuan Wang <zx_will@outlook.com>
2024-12-01 22:05:50 +0000

    clean up

---
+++ script.js
@@ -0,0 +1,63 @@
+function loadMap() {
+  var map = document.getElementById("map").contentDocument.querySelector("svg");
+  var toolTip = document.getElementById("toolTip");
+
+  // Add event listeners to map element
+  if (
+    !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    // If user agent is not mobile add click listener (for wikidata links)
+    map.addEventListener("click", handleClick, false);
+  }
+  map.addEventListener("mousemove", mouseEntered, false);
+  map.addEventListener("mouseout", mouseGone, false);
+
+  // Show tooltip on mousemove
+  function mouseEntered(e) {
+    var target = e.target;
+    if (target.nodeName == "path") {
+      target.style.opacity = 0.6;
+      var details = e.target.attributes;
+
+      // Follow cursor
+      toolTip.style.transform = `translate(${e.offsetX}px, ${e.offsetY}px)`;
+
+      // Tooltip data
+      toolTip.innerHTML = `
+        <ul>
+            <li><b>Province: ${details.name.value}</b></li>
+            <li>Local name: ${details.gn_name.value}</li>
+            <li>Country: ${details.admin.value}</li>
+            <li>Postal: ${details.postal.value}</li>
+        </ul>`;
+    }
+  }
+
+  // Clear tooltip on mouseout
+  function mouseGone(e) {
+    var target = e.target;
+    if (target.nodeName == "path") {
+      target.style.opacity = 1;
+      toolTip.innerHTML = "";
+    }
+  }
+
+  // Go to wikidata page onclick
+  function handleClick(e) {
+    if (e.target.nodeName == "path") {
+      var details = e.target.attributes;
+      window.open(
+        `https://www.wikidata.org/wiki/${details.wikidataid.value}`,
+        "_blank"
+      );
+    }
+  }
+}
+
+// Calls init function on window load
+window.onload = function () {
+  // Init map
+  loadMap();
+};
